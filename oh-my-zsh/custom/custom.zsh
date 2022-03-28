70382087959e7d8fcfbaf331c0d5ae7a7e26d039
Chris Eldredge <chris.eldredge@gmail.com>
2022-03-28 11:34:21 -0400

    Alias getclip/putclip on X11 systems

--- oh-my-zsh/custom/custom.zsh
+++ oh-my-zsh/custom/custom.zsh
@@ -1,2 +1,8 @@
 ZSH_THEME="farmber"
 DISABLE_UNTRACKED_FILES_DIRTY="true"
+
+if type xclip &>/dev/null; then
+  alias getclip='xclip -o -selection clipboard'
+  alias putclip='xclip -i -selection clipboard'
+fi
+
