2f425ca4b90c598fdd9e4dc1fd209011d9887c5a
Chris Eldredge <chris.eldredge@gmail.com>
2025-03-13 11:46:29 -0400

    More improvements to args in assume-role

--- oh-my-zsh/custom/assume-role.zsh
+++ oh-my-zsh/custom/assume-role.zsh
@@ -5,36 +5,85 @@ deactivate-assumed-role() {
 }
 
 assume-role() {
-    local ROLE_ARN=$1
-    local SESSION_NAME="${2:-cveld-local-dev}"
-    local POLICY="${3}"
-
+    local ROLE=""
+    local SESSION_NAME="cveld-local-dev"
+    local POLICY=""
     local EXPORT_VARS=false
     local PRINT_VARS=false
-    for arg in "$@"; do
-        if [[ "$arg" == "--deactivate-first" ]]; then
-            deactivate-assumed-role
-        elif [[ "$arg" == "--export" ]]; then
-            EXPORT_VARS=true
-        elif [[ "$arg" == "--print" ]]; then
-            PRINT_VARS=true
-        fi
+    local DEACTIVATE_FIRST=false
+    local VERBOSE=false
+
+    # Parse named arguments
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+            --session-name)
+                SESSION_NAME="$2"
+                shift 2
+                ;;
+            --policy)
+                POLICY="$2"
+                shift 2
+                ;;
+            --deactivate-first)
+                DEACTIVATE_FIRST=true
+                shift
+                ;;
+            --export)
+                EXPORT_VARS=true
+                shift
+                ;;
+            --print)
+                PRINT_VARS=true
+                shift
+                ;;
+            --verbose)
+                VERBOSE=true
+                shift
+                ;;
+            *)
+                # First non-flag argument is the role ARN or name
+                if [[ -z "$ROLE_ARN" ]]; then
+                    ROLE="$1"
+                else
+                    echo "Unexpected argument: $1" >&2
+                    return 1
+                fi
+                shift
+                ;;
+        esac
     done
 
-    if [[ "$ROLE_ARN" != $'arn:'* ]]; then
-        ROLE_ARN=$(aws iam list-roles | jq -r '.Roles[] | select(.RoleName | contains("'$1'")).Arn')
-        N=$(echo -en $ROLE_ARN | wc -l | tr -d ' ')
-
-        if [ -z "$ROLE_ARN" ]; then
-            echo "No role found matching $1" >&2
-            return 1
-        elif ((N>1)); then
-            echo "Found $N roles matching $1:\n$ROLE_ARN" >&2
-            return 1
-        fi
+    if [[ -z "$ROLE" ]]; then
+        echo "Error: Role ARN or name is required" >&2
+        return 1
     fi
 
-    echo Assuming role $ROLE_ARN with session name $SESSION_NAME
+    if $DEACTIVATE_FIRST; then
+        deactivate-assumed-role
+    fi
+
+    local ROLE_ARN
+
+    if [[ "$ROLE" != $'arn:'* ]]; then
+        ROLE_ARNS=$(aws iam list-roles | jq -r '.Roles[] | select(.RoleName | contains("'$ROLE'")).Arn')
+        local N=$(echo -en $ROLE | wc -l | tr -d ' ')
+
+        if [ -z "$ROLE_ARNS" ]; then
+            echo "No role found matching ${ROLE}" >&2
+            return 1
+        elif ((N>1)); then
+            echo "Found $N roles matching ${ROLE}:\n$ROLE_ARNS" >&2
+            return 1
+        fi
+
+        ROLE_ARN=${ROLE_ARNS}
+    else
+        ROLE_ARN=ROLE
+    fi
+
+    if $VERBOSE; then
+        echo Assuming role $ROLE_ARN with session name $SESSION_NAME
+    fi
 
     local ARGS=("sts" "assume-role" "--role-arn" "$ROLE_ARN" "--role-session-name" "$SESSION_NAME" "--output" "json")
 
@@ -43,25 +92,30 @@ assume-role() {
         ARGS+="${POLICY}"
     fi
 
-    echo aws "${ARGS[@]}"
+    if $VERBOSE; then
+        echo aws "${ARGS[@]}"
+    fi
+
     local ROLE_JSON=$(aws "${ARGS[@]}")
     local RES=$?
     if ((RES!=0)); then
         return $RES
     fi
 
-    local AWS_ACCESS_KEY_ID=$(echo $ROLE_JSON | jq -r '.Credentials''.AccessKeyId')
-    local AWS_SECRET_ACCESS_KEY=$(echo $ROLE_JSON | jq -r '.Credentials''.SecretAccessKey')
-    local AWS_SESSION_TOKEN=$(echo $ROLE_JSON | jq -r '.Credentials''.SessionToken')
+    local LOCAL_AWS_ACCESS_KEY_ID=$(echo $ROLE_JSON | jq -r '.Credentials''.AccessKeyId')
+    local LOCAL_AWS_SECRET_ACCESS_KEY=$(echo $ROLE_JSON | jq -r '.Credentials''.SecretAccessKey')
+    local LOCAL_AWS_SESSION_TOKEN=$(echo $ROLE_JSON | jq -r '.Credentials''.SessionToken')
 
     if $EXPORT_VARS; then
         echo "export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN"
-        export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN
+        export AWS_ACCESS_KEY_ID=$LOCAL_AWS_ACCESS_KEY_ID \
+           AWS_SECRET_ACCESS_KEY=$LOCAL_AWS_SECRET_ACCESS_KEY \
+           AWS_SESSION_TOKEN=$LOCAL_AWS_SESSION_TOKEN
     fi
 
     if $PRINT_VARS; then
-        echo "AWS_ACCESS_KEY_ID=\"${AWS_ACCESS_KEY_ID}\""
-        echo "AWS_SECRET_ACCESS_KEY=\"${AWS_SECRET_ACCESS_KEY}\""
-        echo "AWS_SESSION_TOKEN=\"${AWS_SESSION_TOKEN}\""
+        echo "AWS_ACCESS_KEY_ID=\"${LOCAL_AWS_ACCESS_KEY_ID}\""
+        echo "AWS_SECRET_ACCESS_KEY=\"${LOCAL_AWS_SECRET_ACCESS_KEY}\""
+        echo "AWS_SESSION_TOKEN=\"${LOCAL_AWS_SESSION_TOKEN}\""
     fi
 }
