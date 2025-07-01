00ca74b4a6b987f780fd42145d07b57bf92dcf66
Chris Eldredge <chris.eldredge@gmail.com>
2025-07-01 15:35:50 -0400

    Add zulu function to convert unix epoch times

--- oh-my-zsh/custom/epoch.zsh
+++ oh-my-zsh/custom/epoch.zsh
@@ -4,5 +4,24 @@ epoch() {
         DATE_CMD=gdate
     fi
 
-    $DATE_CMD --date="$1" -u +"%s"
+    local input="$1"
+    if [[ -z "$input" || "$input" == "-" ]]; then
+        input=$(cat)
+    fi
+
+    $DATE_CMD --date="$input" -u +"%s"
+}
+
+zulu() {
+    DATE_CMD=date
+    if type gdate &>/dev/null; then
+        DATE_CMD=gdate
+    fi
+
+    local input="$1"
+    if [[ -z "$input" || "$input" == "-" ]]; then
+        input=$(cat)
+    fi
+
+    $DATE_CMD --date="@${input}" -u --iso-8601=seconds
 }
